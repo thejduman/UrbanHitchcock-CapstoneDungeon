a5798a88e32dd7324da7170da5f69cb85afd3364
thejduman <68288652+thejduman@users.noreply.github.com>
2023-02-24 17:35:15 -0500

    Basic implementation of the quiz game Scripts have been created for the basic functionality of the quiz game. Currently there is a bug where the correct answer is not being set correctly.

--- Assets/Scripts/QuizGame/QuizManager.cs
+++ Assets/Scripts/QuizGame/QuizManager.cs
@@ -1,8 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class QuizManager : MonoBehaviour
 {
-    public List<QuestionsAndAnswers> QnA;
+    public List<QuestionsAndAnswers> QnA; //list of questions
+    public GameObject[] options; //the answer buttons
+    public int currentQuestion; //the current question
+
+    public TMPro.TextMeshProUGUI QuestionText;
+
+    //runs at start of game
+    private void Start()
+    {
+        generateQuestion();
+    }
+
+    public void correct()
+    {
+        QnA.RemoveAt(currentQuestion);
+        generateQuestion();
+    }
+
+    //set the answer text on the buttons
+    void SetAnswers()
+    {
+        for (int i = 0; i < options.Length; i++) 
+        {
+            options[i].GetComponent<AnswerScript>().isCorrect = false;
+            options[i].transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = QnA[currentQuestion].Answers[i];
+
+            if (QnA[currentQuestion].CorrectAnswer == i+1)
+            {
+                options[i].GetComponent<AnswerScript>().isCorrect = true;
+            }
+        }
+    }
+
+    //set the question
+    void generateQuestion()
+    {
+        currentQuestion = Random.Range(0, QnA.Count); //pick a random question from the list
+
+        //set up the quiz
+        QuestionText.text = QnA[currentQuestion].Question;
+        SetAnswers();
+
+    }
 }
