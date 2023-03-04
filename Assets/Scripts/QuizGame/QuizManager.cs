5fa87aca698beedfad7aff6a69ae6805d88a2610
thejduman <68288652+thejduman@users.noreply.github.com>
2023-03-04 14:56:04 -0500

    Added score tracking to the quiz game The user is given visual feedback of how many questions they have answered correctly or incorrectly with text on the screen.

--- Assets/Scripts/QuizGame/QuizManager.cs
+++ Assets/Scripts/QuizGame/QuizManager.cs
@@ -9,16 +9,33 @@ public class QuizManager : MonoBehaviour
     public GameObject[] options; //the answer buttons
     public int currentQuestion; //the current question
 
-    public TMPro.TextMeshProUGUI QuestionText;
+    int totalQuestions = 0; //number of questions in the array
+    public int correctans; //number of times a question is answered correctly
+    public int incorrectans; //number of times a question is answered incorrectly
+
+    public TMPro.TextMeshProUGUI QuestionText; //game object for the question text
+    public TMPro.TextMeshProUGUI CorrectText; //on screen text for the number of correct answers
+    public TMPro.TextMeshProUGUI IncorrectText; //on screen text for the number of incorrect answers
 
     //runs at start of game
     private void Start()
     {
+        totalQuestions = QnA.Count;
         generateQuestion();
     }
 
+    //when a auestion is answered correctly
     public void correct()
     {
+        correctans++;
+        QnA.RemoveAt(currentQuestion);
+        generateQuestion();
+    }
+
+    //when a question is answered incorrectly
+    public void incorrect()
+    {
+        incorrectans++;
         QnA.RemoveAt(currentQuestion);
         generateQuestion();
     }
@@ -31,16 +48,19 @@ public class QuizManager : MonoBehaviour
             options[i].GetComponent<AnswerScript>().isCorrect = false;
             options[i].transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = QnA[currentQuestion].Answers[i];
 
-            if (QnA[currentQuestion].CorrectAnswer == i)
+            if (QnA[currentQuestion].CorrectAnswer == i) //setting the correct answer
             {
                 options[i].GetComponent<AnswerScript>().isCorrect = true;
             }
         }
     }
 
-    //set the question
+    //set the question. runs after a question is answered
     void generateQuestion()
     {
+        CorrectText.text = "Correct: " + correctans;
+        IncorrectText.text = "Incorrect: " + incorrectans;
+        
         if(QnA.Count > 0)
         {
             currentQuestion = Random.Range(0, QnA.Count); //pick a random question from the list
