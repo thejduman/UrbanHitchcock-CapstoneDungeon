992de6567c91d9087ac5543dd068cbefd22cd73b
thejduman <68288652+thejduman@users.noreply.github.com>
2023-04-30 14:12:45 -0400

    Created working inventory system

---
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public GameObject slotPrefab;
+    public List<InventorySlot> inventorySlots = new List<InventorySlot>(13);
+
+    private void OnEnable()
+    {
+        Inventory.OnInventoryChange += DrawInventory;
+    }
+
+    private void OnDisable()
+    {
+        Inventory.OnInventoryChange -= DrawInventory;
+    }
+
+    void ResetInventory()
+    {
+        foreach(Transform childTransform in transform) 
+        {
+            Destroy(childTransform.gameObject);
+        }
+        inventorySlots = new List<InventorySlot>(13);
+    }
+
+    void DrawInventory(List<InventoryItem> inventory)
+    {
+        ResetInventory();
+
+        for (int i = 0; i< inventory.Capacity; i++)
+        {
+            CreateInventorySlot();
+        }
+
+        for (int i = 0; i < inventory.Count; i++)
+        {
+            inventorySlots[i].DrawSlot(inventory[i]);
+        }
+    }
+
+    void CreateInventorySlot()
+    {
+        GameObject newSlot = Instantiate(slotPrefab);
+        newSlot.transform.SetParent(transform, false);
+
+        InventorySlot newSlotComponent = newSlot.GetComponent<InventorySlot>();
+        newSlotComponent.ClearSlot();
+
+        inventorySlots.Add(newSlotComponent);
+    }
+}
