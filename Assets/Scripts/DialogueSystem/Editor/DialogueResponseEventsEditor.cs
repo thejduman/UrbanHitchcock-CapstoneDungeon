1dadaeda0d1416e2fa804b22c0f5efcbd24f1f1e
MysticShadow1307 <71941643+MysticShadow1307@users.noreply.github.com>
2023-04-30 20:50:07 -0400

    IMproved Dialogue System implemented. Response Commands now activate quiz game and collection

---
+++ Assets/Scripts/DialogueSystem/Editor/DialogueResponseEventsEditor.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(DialogueResponseEvents))]
+
+public class DialogueResponseEventsEditor : Editor
+{
+    // Start is called before the first frame update
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        DialogueResponseEvents responseEvents = (DialogueResponseEvents)target;
+
+        if (GUILayout.Button("Refresh"))
+        {
+            responseEvents.OnValidate();
+        }
+    }
+}
